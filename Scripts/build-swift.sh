34f961552a75d548dd9d7570a8dccf3e177781c7
noppoman <yuki@miketokyo.com>
2017-05-31 20:31:40 +0900

    update package before build

--- Scripts/build-swift.sh
+++ Scripts/build-swift.sh
@@ -7,4 +7,5 @@ cp /usr/lib/x86_64-linux-gnu/libicudata.so $DEST/libicudata.so.52
 cp /usr/lib/x86_64-linux-gnu/libicui18n.so $DEST/libicui18n.so.52
 cp /usr/lib/x86_64-linux-gnu/libicuuc.so $DEST/libicuuc.so.52
 cp /usr/lib/x86_64-linux-gnu/libbsd.so $DEST/libbsd.so.0
+swift package update
 swift build
