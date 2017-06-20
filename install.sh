feef378e0cb9614c617355265a5530ba8d4e7667
noppoman <yuki@miketokyo.com>
2017-06-20 19:43:49 +0900

    change download branch to master

--- install.sh
+++ install.sh
@@ -3,8 +3,13 @@
 UNAME=`uname`;
 HEXAVILLE_HOME=$HOME/.hexaville;
 
+if [ -d "$HEXAVILLE_HOME" ]; then
+  echo "hexaville is already installed"
+  exit 1
+fi
+
 if [[ $UNAME == "Darwin" ]]; then
-  curl -OL https://cdn.rawgit.com/noppoMan/Hexaville/binary-distribution/bin/latest/mac/Hexaville.zip
+  curl -OL https://cdn.rawgit.com/noppoMan/Hexaville/master/bin/latest/mac/Hexaville.zip
 else
   if [[ $UNAME == "Linux" ]]; then
     UBUNTU_RELEASE=`lsb_release -a 2>/dev/null`;
@@ -12,7 +17,7 @@ else
       echo "Unsupported OS"
       exit 1
     else
-      curl -OL https://cdn.rawgit.com/noppoMan/Hexaville/binary-distribution/bin/latest/linux/Hexaville.zip
+      curl -OL https://cdn.rawgit.com/noppoMan/Hexaville/master/bin/latest/linux/Hexaville.zip
     fi
   else
     echo "Unsupported OS"
@@ -20,13 +25,11 @@ else
   fi
 fi
 
-if [ ! -d "$HEXAVILLE_HOME" ]; then
-  mkdir -p $HEXAVILLE_HOME
-  mv Hexaville.zip $HEXAVILLE_HOME
-  cd $HEXAVILLE_HOME
-  unzip -o Hexaville.zip
-  mv Hexaville hexaville
-  RCFILE=`echo ${SHELL##*/}`rc
-  echo "\n" >> $HOME/.${RCFILE}
-  echo "export PATH=\"\$PATH:$HEXAVILLE_HOME\"" >> $HOME/.${RCFILE}
-fi
+mkdir -p $HEXAVILLE_HOME
+mv Hexaville.zip $HEXAVILLE_HOME
+cd $HEXAVILLE_HOME
+unzip -o Hexaville.zip
+mv Hexaville hexaville
+RCFILE=`echo ${SHELL##*/}`rc
+echo "" >> $HOME/.${RCFILE}
+echo "export PATH=\"\$PATH:$HEXAVILLE_HOME\"" >> $HOME/.${RCFILE}
