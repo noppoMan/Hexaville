ed9ccae415444b08aec016d443ec2a7f15ac6d96
noppoman <yuki@miketokyo.com>
2017-07-20 23:37:27 +0900

    make current USER as owner for the shared volume of docker

--- Scripts/build-swift.sh
+++ Scripts/build-swift.sh
@@ -8,3 +8,9 @@ cp /usr/lib/x86_64-linux-gnu/libicudata.so $DEST/libicudata.so.52
 cp /usr/lib/x86_64-linux-gnu/libicui18n.so $DEST/libicui18n.so.52
 cp /usr/lib/x86_64-linux-gnu/libicuuc.so $DEST/libicuuc.so.52
 cp /usr/lib/x86_64-linux-gnu/libbsd.so $DEST/libbsd.so.0
+
+UNAME=`uname`;
+if [[ $UNAME == "Linux" ]]; then
+  id -u $VOLUME_USER &>/dev/null || useradd -ms /bin/bash $VOLUME_USER
+  chown -R $VOLUME_USER:$VOLUME_GROUP $DEST
+fi
