5f476b35206331add155195cb0a127f0ab7c7249
noppoman <yuki@miketokyo.com>
2017-08-17 17:52:16 +0900

    Refactor files around releasing binary distribution

--- Dockerfile
+++ Dockerfile
@@ -29,4 +29,4 @@ WORKDIR Hexaville
 
 RUN mkdir -p .build/release
 
-CMD [./Scripts/zip.sh || exit $?]
+CMD ["/bin/bash", "./Scripts/zip.sh"]
--- Scripts/zip.sh
+++ Scripts/zip.sh
@@ -2,9 +2,16 @@
 
 set -e
 
-DEST=./.build/release
 SWIFTFILE=../${SWIFTFILE}
-/bin/bash -c "source Scripts/build-swift.sh"
+DEST=.build/release
+swift package update
+swift build -c ${BUILD_CONFIGURATION}
+cp -r /${SWIFTFILE}/usr/lib/swift/linux/*.so $DEST
+cp /usr/lib/x86_64-linux-gnu/libicudata.so $DEST/libicudata.so.52
+cp /usr/lib/x86_64-linux-gnu/libicui18n.so $DEST/libicui18n.so.52
+cp /usr/lib/x86_64-linux-gnu/libicuuc.so $DEST/libicuuc.so.52
+cp /usr/lib/x86_64-linux-gnu/libbsd.so $DEST/libbsd.so.0
 cp -r templates $DEST
 cp -r Scripts $DEST
-cd .build/debug && zip Hexaville.zip Hexaville ./*.so ./*.so.* -r templates -r Scripts
+cd $DEST
+zip Hexaville.zip Hexaville ./*.so ./*.so.* -r templates -r Scripts
