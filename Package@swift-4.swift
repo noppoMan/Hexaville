0d98ff56e9525598a8845eee3e8c1c831b9a5989
noppoman <yuki@miketokyo.com>
2017-10-09 03:55:09 +0900

    add Package@swift-4.swift and Bump module verions

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -8,10 +8,10 @@ let package = Package(
         .executable(name: "hexaville", targets: ["Hexaville"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/s3.git", .upToNextMajor(from: "1.0.2")),
-        .package(url: "https://github.com/swift-aws/lambda.git", .upToNextMajor(from: "1.0.2")),
-        .package(url: "https://github.com/swift-aws/iam.git", .upToNextMajor(from: "1.0.2")),
-        .package(url: "https://github.com/swift-aws/apigateway.git", .upToNextMajor(from: "1.0.2")),
+        .package(url: "https://github.com/swift-aws/s3.git", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/swift-aws/lambda.git", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/swift-aws/iam.git", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/swift-aws/apigateway.git", .upToNextMajor(from: "1.0.1")),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", .upToNextMajor(from: "16.0.0")),
         .package(url: "https://github.com/jakeheis/SwiftCLI.git", .upToNextMajor(from: "3.1.0")),
         .package(url: "https://github.com/behrang/YamlSwift.git", .upToNextMajor(from: "3.0.0"))
